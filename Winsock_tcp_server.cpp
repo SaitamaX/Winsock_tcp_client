9052a8b02760d18b1ee2be5c47d2e3c7ac279ecc
SaitamaX <xbw19960123@163.com>
2018-11-26 18:56:35 +0800

    d

--- Winsock_tcp_server.cpp
+++ Winsock_tcp_server.cpp
@@ -4,19 +4,22 @@
 #include "stdafx.h"
 #include <iostream>
 #include <WinSock2.h>
+#include <vector>
+#include <string>
 #pragma comment(lib,"ws2_32.lib")
 
-#define MAXCLIENT 3//允许的最大客户端数目
+#define MAXCLIENT 40//允许的最大客户端数目
 using namespace std;
 
-char recvBuf[100];//接受缓冲区
-char sendBuf[100];//发送缓冲区
-
+char recvBuf[1000];//接受缓冲区
+char sendBuf[1000];//发送缓冲区
 struct SA
 {
 	SOCKET socket;//客户端套接字
 	sockaddr_in addr;//客户端地址（协议、端口号、ip地址）
+	int room_number;//存放当前客户端进入的聊天室
 };
+//vector<SA> cli_list;//用于存放客户端的套接字和地址的表
 
 int iClient = 0;
 SA saCli[MAXCLIENT];
@@ -24,23 +27,39 @@ SA saCli[MAXCLIENT];
 //接收客户端发来的数据的进程
 void recvProc(SA* sa)
 {
-	ZeroMemory(recvBuf, 100);
-	ZeroMemory(sendBuf, 100);
 	while (true)
 	{
-		if (SOCKET_ERROR == recv(sa->socket, recvBuf, sizeof(recvBuf), 0))//接受客户端发来的数据失败
+		ZeroMemory(recvBuf, 1000);
+		ZeroMemory(sendBuf, 1000);
+		if (SOCKET_ERROR == recv(sa->socket, recvBuf,  sizeof(recvBuf), 0))//接受客户端发来的数据失败
 		{
 			cout << inet_ntoa(sa->addr.sin_addr) << "已断开连接" << endl;//只在服务器端显示
 			return;
 		}
 		if (recvBuf[0] != 0)//接受客户端发来的数据成功且数据不为空
 		{
-			sprintf_s(sendBuf, "[群聊]<%s>说：%s", inet_ntoa(sa->addr.sin_addr), recvBuf);
-			cout << sendBuf << endl;//将该客户端发来的信息打印到服务器端的控制台
-			for (int i = 0; i <= iClient; i++)
-			{
-				send(saCli[i].socket, sendBuf, sizeof(sendBuf), 0);//将该客户端发来的数据发送给每一个客户端（包括该客户端自身）
-			}
+			string s = recvBuf;
+			cout << s << endl;
+			//if (flag_first) {
+			//	saCli[iClient].room_number = atoi(recvBuf);//添加当前客户端选择的聊天室
+			//	sa->room_number = atoi(recvBuf);
+			//	flag_first = false;
+			//	sprintf_s(sendBuf, "用户<%s>进入聊天室%s", inet_ntoa(sa->addr.sin_addr), recvBuf);
+			//}
+			//else
+			//	sprintf_s(sendBuf, "[用户]<%s>说：%s", inet_ntoa(sa->addr.sin_addr), recvBuf);
+			//cout << sendBuf << endl;//将该客户端发来的信息打印到服务器端的控制台
+			//for (int i = 0; i < iClient; ++i) {
+			//	//将信息发送到同一房间的其他客户端（自身除外）
+			//	if (saCli[i].room_number == sa->room_number && (saCli[i].addr.sin_port != sa->addr.sin_port 
+			//		|| inet_ntoa(saCli[i].addr.sin_addr) != inet_ntoa(sa->addr.sin_addr))) {
+			//		send(saCli[i].socket, sendBuf, sizeof(sendBuf), 0);
+			//	}
+			//}
+			//else {
+			//	for (int i = 0; i <= iClient; i++)
+			//		send(saCli[i].socket, sendBuf, sizeof(sendBuf), 0);//将此客户端发来的数据发送给每一个客户端（包括该客户端自身）
+			//}
 		}
 	}
 	closesocket(sa->socket);
@@ -51,13 +70,9 @@ void ProcessGetKeyIn()
 {
 	while (true)
 	{
-		char temp[90];
-		ZeroMemory(temp, 90);
-		ZeroMemory(sendBuf, 100);
-		cin >> temp;//服务器端输入数据
-		sprintf_s(sendBuf, "服务器说：%s", temp);
-		cout << sendBuf << endl;
-
+		ZeroMemory(sendBuf, 1000);
+		scanf("%[^\n]", &sendBuf);//服务器端输入数据
+		cout << "服务器说："<<sendBuf << endl;
 		for (int i = 0; i < iClient; i++) {
 			send(saCli[i].socket, sendBuf, sizeof(sendBuf), 0);//数据发送给每一个客户端
 		}
@@ -87,6 +102,8 @@ int _tmain(int argc, _TCHAR* argv[])
 	}
 
 	sockaddr_in addrSer;//服务器地址（协议、端口号、ip）
+	// 0.0.0.0  <=>  127.0.0.1  |  192.168.  | 49
+	// 
 	addrSer.sin_addr.S_un.S_addr = htonl(INADDR_ANY);//ip地址取为INADDR_ANY，以允许服务器应用监听主机上每个网络接口的客户机活动
 	addrSer.sin_family = AF_INET;
 	addrSer.sin_port = htons(8888);//端口号设置为8888
@@ -123,8 +140,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	{
 		sockaddr_in addrCli;//客户端地址
 		int len = sizeof(SOCKADDR);//sockaddr_in结构的长度
-
-								   /*
+	    /*
 								   调用accept函数的目的：使套接字做好接受客户连接的准备。
 
 								   该函数的函数原型：SOCKET accept(SOCKET s,struct sockadddr FAR *addr,int FAR * addrlen);
@@ -142,9 +158,8 @@ int _tmain(int argc, _TCHAR* argv[])
 		if (iClient < MAXCLIENT) {
 			saCli[iClient].socket = socketCon;
 			saCli[iClient].addr = addrCli;
-
-			ZeroMemory(sendBuf, 100);//清空发送缓冲区
-									 /*
+			ZeroMemory(sendBuf, 1000);//清空发送缓冲区
+			/*
 									 函数原型：
 									 int sprintf_s(
 									 char *buffer,
@@ -168,7 +183,6 @@ int _tmain(int argc, _TCHAR* argv[])
 			else {
 				threads[iClient] = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)recvProc, (void*)&saCli[iClient], 0, NULL);//创建一个线程来接收客户端发来的数据
 			}
-
 			iClient++;
 		}
 		else {
